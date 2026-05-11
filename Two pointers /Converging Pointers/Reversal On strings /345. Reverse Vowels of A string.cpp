c896491566900ca5e7e8c1302b32042ee982885b
shekhartomar1 <gmrshekhar007@gmail.com>
2026-05-11 16:30:37 +0530

    Create 345. Reverse Vowels of A string.cpp

---
+++ Two pointers /Converging Pointers/Reversal On strings /345. Reverse Vowels of A string.cpp
@@ -0,0 +1,20 @@
+// same wohi condition based converging pointers type pattern
+
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int l = 0;
+        int r = s.size() -1;
+        
+        while(l<r){
+            while(l < r && !isalpha(s[l]) ) l++;
+            while(l < r && !isalpha(s[r]) ) r--;
+            
+            swap(s[l] , s[r]);
+            l++;
+            r--;
+        }
+        return s;
+    }
+};
+
