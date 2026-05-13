2defa8cf20182735d33a02ba3f0a002d51a41514
shekhartomar1 <gmrshekhar007@gmail.com>
2026-05-13 15:40:20 +0530

    Create 283. Move zeroes to the end.cpp

---
+++ Two pointers /Slow and Fast Pointers/Filtering the data structure/283. Move zeroes to the end.cpp
@@ -0,0 +1,31 @@
+// iss problem me statement read krrnne se idea lena 
+//  kya idea ?? idea ye ke isme khi nhi khi data structure ko filter krrne ki baat ho rhi hai
+// kesi baat ?? hame khi na khi sabhi zeroes ka end mein ya sabhi non-zero elements ko front mein push krrna hai
+// fata ko filter krrna hai toh ham wohi two pointers ki slow and fast moving pointers ka use krrneg 
+// jisme conventionally i ko ham apne filtered array ko mark krrne ke liye use krrenge aur j ko iteration ke liye 
+// ham kisi condition ko fullfill krrenge using the values we will get from these 2 indexes 
+
+
+// specifically move zero jaisi problem ka ham extra space use krke bhi krr sakte hai , jaise non zero elements ko 
+// pehle fill krrlo iterate krrke aur bache hue elements mein zero daal do
+
+// samme kaam sort array by parity problem mein bhi kiya jaa sakta hai , jha even numbers front mein aur off end mein
+ 
+     |||
+     |||
+     |||
+    \   /
+     \ /
+      V
+
+        int i = 0;
+        
+        while( i < nums.size() && nums[i] != 0) i++;
+        int j = i+1;
+        
+        for(; j< nums.size() ; j++){
+            if(nums[j] != 0){
+                swap(nums[j] , nums[i]);
+                i++;
+            }
+        }
